a577c8df9c57ab7bfe8daa19c95e787dc96c3320
Luiz Renan <110012412+luiznwz@users.noreply.github.com>
2023-11-17 14:45:18 -0300

    add js

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
 import './scss/style.scss';
 
+const btnOpen = document.querySelector('#btnOpen');
+const media = window.matchMedia('(width < 69.375em)');
+
+function openMobileMenu() {
+    console.log('open menu');
+
+    btnOpen.setAttribute('aria-expanded', 'true');
+}
+
+btnOpen.addEventListener('click', openMobileMenu);
+
+media.addEventListener('change', function(e){
+    console.log('window.matchMedia change = ' + e.matches);
+})
 
 
