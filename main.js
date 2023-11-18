311270819882f71d97cfa5b3b0004626a2a4f480
Luiz Renan <110012412+luiznwz@users.noreply.github.com>
2023-11-17 23:57:05 -0300

    solved

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import './scss/style.scss';
 
 const btnOpen = document.querySelector('#btnOpen');
 const media = window.matchMedia('(width < 69.375em)');
+const navContent = document.querySelector('.nav__content');
+const navOverlay = document.querySelector('.nav__overlay');
 
 function openMobileMenu() {
     console.log('open menu');
@@ -9,10 +11,27 @@ function openMobileMenu() {
     btnOpen.setAttribute('aria-expanded', 'true');
 }
 
+function setupNav(e){
+    if (e.matches) { // mobile
+        console.log('is mobile');
+
+        setTimeout(() => {
+            navContent.style.display = 'block';
+            navOverlay.style.display = 'block';
+          }, 500);
+    } else {
+        //desktop
+        console.log('is desktop');
+        navContent.style.display = '';
+    }
+}
+
+setupNav(media);
+
 btnOpen.addEventListener('click', openMobileMenu);
 
-media.addEventListener('change', function(e){
-    console.log('window.matchMedia change = ' + e.matches);
-})
+media.addEventListener('change', function (e) {
+   setupNav(e);
+});
 
 
